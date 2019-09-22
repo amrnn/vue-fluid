49be1ae8e1aa66de3d6baa0fd7ca83c410ee16cf
Amr Noman <amrnn90@gmail.com>
2019-09-22 15:14:32 +0300

    support SSR

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,9 @@ mix.webpackConfig({
 	output: {
 		library: 'vue-fluid',
 		libraryTarget: 'umd',
-		umdNamedDefine: true
-	}
+		umdNamedDefine: true,
+
+		// Needed for usage with SSR, see:https://stackoverflow.com/questions/49111086/webpack-4-universal-library-target
+		globalObject: "typeof self !== 'undefined' ? self : this"
+	},
 });
